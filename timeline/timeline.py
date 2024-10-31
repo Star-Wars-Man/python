701399b1cca4df7a7660e0b64495aef4b303672c
Star-Wars-Man <jacob@olsenweb.net>
2024-10-31 09:10:11 -0600

    i broke it

--- timeline/timeline.py
+++ timeline/timeline.py
@@ -2,19 +2,19 @@ print("Hello Timeline")
 
 # start game
     # 1. game menu
-    # 1.1 select number of players
-    # 1.2 [bots on/of]
-    # 1.3 [dificultly levle]
+        # 1.1 select number of players
+        # 1.2 [bots on/of]
+        # 1.3 [dificultly levle]
     # 2. setup new game
-    # 2.1 deal starting card to each player
-    # 2.2 show instructions (tell how to play game)
-    # 2.3 pick starting game
+        # 2.1 deal starting card to each player
+        # 2.2 show instructions (tell how to play game)
+        # 2.3 pick starting game
     # 3. play game (player turns)
-    # 3.1 starting player turn
-    # 3.2 select next player
-    # 3.3 take turn
-    # 3.4 check if player has won/game is over, if so goto 4
-    # 3.5 goto 3.2
+        # 3.1 starting player turn
+        # 3.2 select next player
+        # 3.3 take turn
+        # 3.4 check if player has won/game is over, if so goto 4
+        # 3.5 goto 3.2
     # 4 show who winner is
 def timeline():
     print("Starting timeline game")
@@ -24,7 +24,14 @@ def timeline():
     showWinner()
 
 def showGameMenu():
-    print("Game Menu")
+    player_score = {}
+    new_player = True
+    while new_player:
+        player_name = input("Player Name: ")
+        player_score.update(player_name : 0)
+        another = input("Add Player Y or N?: ")
+        if another == "N" or "n" or "no" or "No":
+            new_player = False
 
 def setUpNewGame():
     print("New Game")
